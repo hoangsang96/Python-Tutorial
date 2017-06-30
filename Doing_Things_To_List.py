8532fbf76f02ad82c40deeaaef72404f1d254251
hoangsang96 <nguyenhoangsang6014@gmail.com>
2017-06-30 14:22:39 +0700

    List

---
+++ Doing_Things_To_List.py
@@ -0,0 +1,18 @@
+ten_things = "Apples Oranges Crows Telephone Light Sugar"
+print("Wait there's not 10 things in that list, let's fix that.")
+stuff = ten_things.split(' ')
+
+more_stuff = ["Day", "Thing", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
+
+while len(stuff) != 10:
+	next_one = more_stuff.pop()
+	print("Adding ", next_one)
+	stuff.append(next_one)
+	print("These %d item now", len(stuff))
+
+print("There we go ", stuff)
+print(stuff[1])
+print(stuff[-1])
+print(stuff.pop())
+print(" ".join(stuff))
+print("#".join(stuff[3:5]))
